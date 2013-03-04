8315744b0329c31b0ee789aa964f3bfc28cbfcb5
robsmall <robsmall@umich.edu>
2013-03-04 15:52:53 -0500

    Attemted to do some java stuff...Need a bit of help with the compiler errors

---
+++ src/com/android/hubbahubba/AddLocation.java
@@ -0,0 +1,20 @@
+package com.android.hubbahubba;
+
+import android.os.Bundle;
+import android.app.Activity;
+//import android.view.Menu;
+
+public class AddLocation extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_location);
+    }
+
+    //@Override
+    //public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater().inflate(R.menu.activity_sign_up, menu);
+        //return true;
+    //}
+}
