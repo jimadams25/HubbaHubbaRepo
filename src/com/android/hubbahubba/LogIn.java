3acdc3d3d103ce147f0c65de623050bad3b0614c
robsmall <robsmall@umich.edu>
2013-03-05 16:17:31 -0500

    Updated sign up, sign in, and linked some pages together

--- src/com/android/hubbahubba/LogIn.java
+++ src/com/android/hubbahubba/LogIn.java
@@ -2,7 +2,10 @@ package com.android.hubbahubba;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class LogIn extends Activity {
 
@@ -10,6 +13,27 @@ public class LogIn extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        
+        //create back button
+        Button backButton = (Button)findViewById(R.id.backButton);
+        Button logInButton = (Button)findViewById(R.id.logInButton);
+        		
+        //back button goes back to the main page
+        backButton.setOnClickListener(new View.OnClickListener() {      
+        	public void onClick(View view) {
+        		Intent intent = new Intent(LogIn.this, MainActivity.class);
+        		startActivity(intent);
+        	}
+
+        });
+        
+        //login Button takes you to the map view
+        logInButton.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View view){
+        		Intent intent = new Intent(LogIn.this, ViewMap.class);
+        		startActivity(intent);
+        	}
+        });
     }
 
     @Override
--- src/com/android/hubbahubba/SignUp.java
+++ src/com/android/hubbahubba/SignUp.java
@@ -2,7 +2,10 @@ package com.android.hubbahubba;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends Activity {
 
@@ -10,6 +13,27 @@ public class SignUp extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        
+        //create back button
+        Button backButton = (Button)findViewById(R.id.backButton);
+        Button logInButton = (Button)findViewById(R.id.SignUpButton);
+        		
+        //back button goes back to the main page
+        backButton.setOnClickListener(new View.OnClickListener() {      
+        	public void onClick(View view) {
+        		Intent intent = new Intent(SignUp.this, MainActivity.class);
+        		startActivity(intent);
+        	}
+
+        });
+        
+        //login Button takes you to the map view
+        logInButton.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View view){
+        		Intent intent = new Intent(SignUp.this, ViewMap.class);
+        		startActivity(intent);
+        	}
+        });
     }
 
     @Override
